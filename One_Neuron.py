27588627bb7d5696e5346db81c79407e76875f3f
Nithish_Yadav <72069347+Nithish-Yadav-31@users.noreply.github.com>
2024-01-08 12:02:36 +0530

    Added code for one neuron

---
+++ One_Neuron.py
@@ -0,0 +1,8 @@
+inputs = [1.2, 1.6, 2.4]
+weights = [3.1, 1.8, 2.9]
+bias = 3
+
+## Linear Operation performed on one single neuron
+output = inputs[0] * weights[0] + inputs[1] * weights[1] + inputs[2] * weights[2] + bias
+
+print(output)
